b0c96d4c6e9acc0780d50a48f7e7161439716549
golan2 <golan2@hotmail.com>
2017-12-04 13:13:54 +0200

    MirrorTree - switch to use BST because it is more coherent to understand how the tree looks like when you read the code

--- src/main/java/interview/trees/MirrorTree.java
+++ src/main/java/interview/trees/MirrorTree.java
@@ -1,74 +1,42 @@
 package interview.trees;
 
+import interview.trees.BST.Node;
+
 public class MirrorTree {
 
     public static void main(String[] args) {
-
-        final Node n7 = new Node(7);
-        final Node n8 = new Node(8);
-
-        final Node n6 = new Node(6, n7, n8);
-        final Node n5 = new Node(5);
-        final Node n4 = new Node(4);
-
-        final Node n3 = new Node(3, null, n6);
-        final Node n2 = new Node(2, n4, n5);
-
-        final Node n1 = new Node(1, n2, n3);
-
-        System.out.println(printTree(n1));
-        flip(n1);
-        System.out.println(printTree(n1));
-
+        final BST t = createTree1();
+        System.out.println("BEFORE: \n" + t);
+        mirror(t.root);
+        System.out.println("AFTER: \n" + t);
     }
 
-    private static void flip(Node root) {
-        if (root==null) {
-            return;
-        }
+    /**
+     * I use a BST because the order of insertion defines the structure of the tree.
+     * I don't care about the search capabilities just to get a tree with a desired shape.
+     */
+    private static MyIntBST createTree1() {
+        final MyIntBST tree = new MyIntBST();
+        tree.put(10);
+        tree.put(8);
+        tree.put(7);
+        tree.put(9);
+        tree.put(20);
+        tree.put(30);
+        tree.put(25);
+        tree.put(35);
+        return tree;
+    }
+
+    private static void mirror(Node root) {
+        if (root==null) return;
 
         Node t = root.left;
         root.left = root.right;
         root.right = t;
 
-        flip(root.left);
-        flip(root.right);
+        mirror(root.left);
+        mirror(root.right);
     }
 
-    private static class Node {
-        String val;
-        Node left;
-        Node right;
-
-        public Node(int val) {
-            this.val = ""+val;
-        }
-
-        public Node(int val, Node left, Node right) {
-            this(val);
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    private static String printTree(Node root) {
-        StringBuilder res = new StringBuilder();
-
-        toJsonString(root, res);
-
-        return res.toString();
-    }
-
-    private static void toJsonString(Node node, StringBuilder buf) {
-        if (node==null) {
-            buf.append("null");
-            return;
-        }
-
-        buf.append("{\"Node\":{\"key\": ").append(node.val).append(" , \"left\" : ");
-        toJsonString(node.left, buf);
-        buf.append(" , \"right\" : ");
-        toJsonString(node.right, buf);
-        buf.append(" }}");
-    }
 }
