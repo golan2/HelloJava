f76589da212f3e87102dc8a2c164ffeee4a77a62
Izik Golan <golan2@hotmail.com>
2022-01-18 00:53:07 +0200

    IsomorphicStrings

--- src/main/java/strings/BijectionMap.java
+++ src/main/java/strings/BijectionMap.java
@@ -1,5 +1,7 @@
 package strings;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,12 +9,13 @@ import java.util.Map;
  * A map from a key to value and from value to map.
  * Allows to quickly check if a value exists or to get the key for a certain value.
  */
+@Slf4j
 class BijectionMap<K, V> {
     final Map<K, V> map = new HashMap<>();
     final Map<V, K> inverseMap = new HashMap<>();
 
-
     public void put(K key, V value) {
+        log.debug("{} => {}", key, value);
         map.put(key, value);
         inverseMap.put(value, key);
     }
